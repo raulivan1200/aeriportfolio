6c59d45be523f28c10449805e8c48be9c2b1746e
RaulRivera <rirpgamer.12@hotmail.com>
2023-06-27 14:50:47 -0700

    cahnges

--- src/components/Menu/Menupj.js
+++ src/components/Menu/Menupj.js
@@ -3,19 +3,16 @@ import SwitchTheme from '../SwitchTheme'
 const Menupj = () => {
   return (
     <div className='menux'>
-        <div className='izqx'>
-            <a href="/" style={{textDecoration:"none"}}>
-                 <div style={{display:"flex",flexDirection:"row !important"}}>
-                 <div style={{ display: "flex" }}>
-    <svg className="svco ssm" xmlns="http://www.w3.org/2000/svg" height="auto" viewBox="0 -960 960 960" width="20">
-      <path d="M400-80 0-480l400-400 56 57-343 343 343 343-56 57Z" />
-    </svg>
-  </div>
-
-        <h6 className='wh' style={{textShadow:"-1px 0px var(--white)"}}>Back</h6>          
-                 </div>
-                </a>
-        </div>
+<div className='izqx'>
+  <a href="/" style={{textDecoration: "none", display: "flex", alignItems: "center"}}>
+    <div style={{display: "flex"}}>
+      <svg className="svco ssm" xmlns="http://www.w3.org/2000/svg" height="auto" viewBox="0 -960 960 960" width="20">
+        <path d="M400-80 0-480l400-400 56 57-343 343 343 343-56 57Z" />
+      </svg>
+    </div>
+    <h6 className='wh' style={{textShadow: "-1px 0px 1px var(--white)"}}>Back</h6>
+  </a>
+</div>
         <div className='derx' >
         <SwitchTheme/>
         </div>
