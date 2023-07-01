05713aac6e622d5ac9a4938a3ad4f63d838f1a4b
RaulRivera <rirpgamer.12@hotmail.com>
2023-06-30 17:23:30 -0700

    more variables

--- src/components/header/Head.js
+++ src/components/header/Head.js
@@ -1,40 +1,20 @@
 import React, { useEffect, useState } from 'react';
 
 function isIOS() {
-  return /iPad|iPhone|iPod/.test(navigator.platform) ||
-    (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
+  const iOSDevices = /iPad|iPhone|iPod/.test(navigator.platform);
+  const isMac = navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1;
+  const iOSVersions = /(iPhone|iPod|iPad).*OS\s([0-9]{1,2})_/i.exec(navigator.userAgent);
+  const iOSVersion = iOSVersions ? parseInt(iOSVersions[2], 10) : 0;
+
+  return iOSDevices || isMac || iOSVersion >= 15 || iOSVersion === 17 || iOSVersion === 16;
 }
+
 
 function AppleComponent() {
   return <div>
     <h1>
-
     This is the Apple component. <br/>
     This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-    This is the Apple component. <br/>
-  
     </h1>
   </div>;
 }
@@ -42,16 +22,6 @@ function AppleComponent() {
 function NonAppleComponent() {
   return <div>
     <h1 >
-
-    This is the Non-Apple component. <br/>
-    This is the Non-Apple component. <br/>
-    This is the Non-Apple component. <br/>
-    This is the Non-Apple component. <br/>
-    This is the Non-Apple component. <br/>
-    This is the Non-Apple component. <br/>
-    This is the Non-Apple component. <br/>
-    This is the Non-Apple component. <br/>
-    This is the Non-Apple component. <br/>
     This is the Non-Apple component. <br/>
     This is the Non-Apple component. <br/>
     </h1>
