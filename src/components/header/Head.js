cdbaffd1601717341cd8be612a96188de0b8f800
RaulRivera <rirpgamer.12@hotmail.com>
2023-06-30 17:34:58 -0700

    more checks

--- src/components/header/Head.js
+++ src/components/header/Head.js
@@ -2,27 +2,29 @@ import React, { useEffect, useState } from 'react';
 
 function isIOS() {
   const iOSDevices = /iPhone|iPad|iPod/.test(navigator.userAgent);
-  const isMac = navigator.userAgent.includes('Macintosh');
+  const isMac = /Mac/.test(navigator.platform);
   const isTouchScreen = navigator.maxTouchPoints > 1;
   const iOSVersions = /(iPhone|iPod|iPad).*OS\s([0-9]{1,2})_/i.exec(navigator.userAgent);
   const iOSVersion = iOSVersions ? parseInt(iOSVersions[2], 10) : 0;
 
   return (
     iOSDevices ||
-    isMac && isTouchScreen ||
+    (isMac && isTouchScreen) ||
     (iOSVersion >= 15 && iOSVersion <= 17) ||
     (iOSVersion === 14 && navigator.userAgent.includes('iPhone14')) ||
     (iOSVersion === 13 && navigator.userAgent.includes('iPhone13')) ||
     (iOSVersion === 12 && navigator.userAgent.includes('iPhone12')) ||
     (iOSVersion === 11 && navigator.userAgent.includes('iPhone11')) ||
-    (isMac && navigator.userAgent.includes('MacIntel') && navigator.userAgent.includes('M1')) ||
-    (isMac && navigator.userAgent.includes('MacIntel') && navigator.userAgent.includes('Apple M2')) ||
-    (isMac && navigator.userAgent.includes('M1') && navigator.userAgent.includes('MacBook Air')) ||
-    (isMac && navigator.userAgent.includes('M1') && navigator.userAgent.includes('MacBook Pro')) ||
-    (isMac && navigator.userAgent.includes('M1') && navigator.userAgent.includes('Mac mini')) ||
-    (isMac && navigator.userAgent.includes('M1') && navigator.userAgent.includes('iMac'))
+    (isMac && navigator.platform.includes('MacIntel') && navigator.userAgent.includes('M1')) ||
+    (isMac && navigator.platform.includes('MacIntel') && navigator.userAgent.includes('Apple M2')) ||
+    (isMac && navigator.platform.includes('MacIntel') && navigator.userAgent.includes('MacBook Air')) ||
+    (isMac && navigator.platform.includes('MacIntel') && navigator.userAgent.includes('MacBook Pro')) ||
+    (isMac && navigator.platform.includes('MacIntel') && navigator.userAgent.includes('Mac mini')) ||
+    (isMac && navigator.platform.includes('MacIntel') && navigator.userAgent.includes('iMac'))
   );
 }
+
+
 function AppleComponent() {
   return <div>
     <h1>
