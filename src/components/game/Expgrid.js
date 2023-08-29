16c8cc71ebea6ffba4f2f6d592b821f592a9630f
RaulRivera <rirpgamer.12@hotmail.com>
2023-08-28 18:37:23 -0700

    hotfix

--- src/components/game/Expgrid.js
+++ src/components/game/Expgrid.js
@@ -4,7 +4,7 @@ function Expgrid() {
   return (
     <div className={styles.parentskilll}>
                 <div className={styles.padlef}>
-                <h1>
+                <h1 className='wh'>
         My Journey
         </h1>
         <hr className={styles.hrpp}/>
@@ -15,18 +15,18 @@ function Expgrid() {
     <div className={styles.containerexpsk}>
         
     <div className={styles.ex1}>
-<h2>Minami Design</h2>
-<h3>Ux Ui Designer</h3>
+<h2 className='wh'>Minami Design</h2>
+<h3 className='wh'>Ux Ui Designer</h3>
     </div>
     <div className={styles.ex2}><h6 className={styles.mcero}> Participate in the design of intuitive and attractive interfaces in more than 14 projects, using methodologies such as SCRUM, Lean design, User Journeys, etc. Design websites and responsive versions, landing pages, Create sitemaps and user journeys. Developed an base design system increasing project development speed by 64%</h6></div>
     <div className={styles.ex3}>
-    <h2>Minami Design</h2>
-<h3>Ux Ui Designer</h3>
+    <h2 className='wh'>Minami Design</h2>
+<h3 className='wh'>Ux Ui Designer</h3>
     </div>
     <div className={styles.ex4}><h6 className={styles.mcero}>Participate in the design of intuitive and attractive interfaces in more than 14 projects, using methodologies such as SCRUM, Lean design, User Journeys, etc. Design websites and responsive versions, landing pages, Create sitemaps and user journeys. Developed an base design system increasing project development speed by 64%</h6></div>
     <div className={styles.ex5}>
-    <h2>Chiquimundo</h2>
-<h3>Web Content designer</h3>
+    <h2 className='wh'>Chiquimundo</h2>
+<h3 className='wh'>Web Content designer</h3>
     </div>
     <div className={styles.ex6}><h6 className={styles.mcero}>Participate in the design of intuitive and attractive interfaces in more than 14 projects, using methodologies such as SCRUM, Lean design, User Journeys, etc. Design websites and responsive versions, landing pages, Create sitemaps and user journeys. Developed an base design system increasing project development speed by 64%</h6></div>
   </div>
