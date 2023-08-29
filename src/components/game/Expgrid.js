fc670c9cf30b44021d669d14f4c9baf32623e69d
RaulRivera <66335304+raulivan1200@users.noreply.github.com>
2023-08-28 19:35:15 -0700

    Update Expgrid.js

--- src/components/game/Expgrid.js
+++ src/components/game/Expgrid.js
@@ -20,15 +20,15 @@ function Expgrid() {
     </div>
     <div className={styles.ex2}><h6 className={styles.mcero}> Participate in the design of intuitive and attractive interfaces in more than 14 projects, using methodologies such as SCRUM, Lean design, User Journeys, etc. Design websites and responsive versions, landing pages, Create sitemaps and user journeys. Developed an base design system increasing project development speed by 64%</h6></div>
     <div className={styles.ex3}>
-    <h2 className='wh'>Minami Design</h2>
+    <h2 className='wh'>Teko Estudio</h2>
 <h3 className='wh'>Ux Ui Designer</h3>
     </div>
-    <div className={styles.ex4}><h6 className={styles.mcero}>Participate in the design of intuitive and attractive interfaces in more than 14 projects, using methodologies such as SCRUM, Lean design, User Journeys, etc. Design websites and responsive versions, landing pages, Create sitemaps and user journeys. Developed an base design system increasing project development speed by 64%</h6></div>
+    <div className={styles.ex4}><h6 className={styles.mcero}>Designed different flows and screens for systems and applications, using Ux and Ui knowledge, exceeding customer, and user expectations. I collaborated closely employing agile communication and project control with the other areas of the company.</h6></div>
     <div className={styles.ex5}>
     <h2 className='wh'>Chiquimundo</h2>
 <h3 className='wh'>Web Content designer</h3>
     </div>
-    <div className={styles.ex6}><h6 className={styles.mcero}>Participate in the design of intuitive and attractive interfaces in more than 14 projects, using methodologies such as SCRUM, Lean design, User Journeys, etc. Design websites and responsive versions, landing pages, Create sitemaps and user journeys. Developed an base design system increasing project development speed by 64%</h6></div>
+    <div className={styles.ex6}><h6 className={styles.mcero}>Content design employing design strategies increasing the companys visibility by 24% considering trends. I led and coordinated content development projectsusing design strategies giving visibility to the company. Create mailing banners.</h6></div>
   </div>
     </div>
     </div>
